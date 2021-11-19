a70b187a33e17dd99eb2acb7176398ac280ca34f
Selam Alem <selamtalem@gmail.com>
2021-11-19 15:34:38 -0600

    Finalized Delete Button Functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,20 +1,23 @@
+
+/////////////////////////////////////////////////////////
 console.log('client.js working!!');
 
 $(document).ready(function(){
     console.log('JQUERY in the house!!');
- 
-    renderTasks();
+ $('#taskShelf').on('click', 'button', deleteTask);
+    renderTasks();//shows all the tasks in the checklist 
+
     
 });
 
 /////////////////////////////////////////////////////////
 
+/////////////////////////START 'renderTasks' FUNCTION/////////////////////////////
 //here we will respond to the GET request from router to grab our entire checklist.
 function renderTasks(tasks) {
     console.log('in clients renderTasks function');
-    
     $('#taskShelf').empty();
-    //console.log('empty taskShelf in clients GET route');
+    //console.log('taskshelf in GET route');
     $.ajax({
         type: 'GET',
         url: '/checklist'
@@ -28,9 +31,26 @@ function renderTasks(tasks) {
                 <td><button class="completedButton" data-id="${task.id}" data-completed-status="${task.completed}">Completed</button> </td>
                 </tr>
             `);
-            
         }
     });
+    
+}
+/////////////////////////END 'renderTasks' FUNCTION//////////////////////////////////////
+
+/////////////////////////START 'deleteTask' FUNCTION//////////////////////////////////////
+//now that we have our table showing from the database, we will try to delete a task:
+function deleteTask() {
+    console.log('Delete button clicked!');
+
+    const taskToDelete = $(this).data('id');//giving the task we delete a value of an /id
+    $.ajax({//here we are requesting our server to delete:
+        type: 'DELETE',
+        url: `/checklist/${taskToDelete}`
+    }).then((respond) => {
+        console.log('working delete function', response);
+        renderTasks();
+        
+    })
     
 }
 
@@ -42,6 +62,19 @@ function renderTasks(tasks) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////END 'deleteTask' FUNCTION//////////////////////////////////////
 
 /////////////////////////////////////////////////////////
 
--- server/routes/checklist.router.js
+++ server/routes/checklist.router.js
@@ -4,13 +4,15 @@ const pg = require('pg');
 const pool = require('../modules/pool.js');//added '.js'
 /////////////////////////////////////////////////////////////////
  
-//creating a request to GET all checklist
+
+/////////////////////////START 'GET' ROUTER//////////////////////////////////////
+//creating a respond to our client to GET all tasks in our checklist
 router.get('/', (req, res) => {
     console.log('in clients GET router ');
 
     let queryText = 'SELECT * FROM "checklist" ORDER BY "task";';
     pool.query(queryText).then(result => {
-        res.send(result.rows)
+        res.send(result.rows)//here we are sending out response from the database to our client
     })
     .catch(error => {
         console.log('Error in client GET router to get all books', error);
@@ -18,6 +20,34 @@ router.get('/', (req, res) => {
  
     });
     
+});
+/////////////////////////END 'GET' ROUTER//////////////////////////////////////
+
+
+/////////////////////////START 'DELETE' ROUTER//////////////////////////////////////
+router.delete('/:id', (req,res) => {//this corresponds to the URL on the client's side
+    //console.log('inside DELETE in router');
+    console.log('req.params: Deleted the following:', req.params);
+    
+    const taskToDelete = req.params.id;
+    
+    const sqlText = `
+    DELETE FROM "checklist"
+    WHERE "id" = $1;
+    `;
+
+    const sqlValue = [taskToDelete];
+
+    pool.query(sqlText, sqlValues)
+    .then((result) => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log('Error in DELETE route', error);
+        res.sendStatus(500);
+        
+    })
+
 });
 
 
@@ -28,21 +58,64 @@ router.get('/', (req, res) => {
 
 
 
-/////////////////////////////////////////////////////////////////
 
 
 
 
 
 
-/////////////////////////////////////////////////////////////////
+
+
+/////////////////////////END 'DELETE' ROUTER//////////////////////////////////////
 
 
 
 
 
 
-/////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////END 'GET' ROUTER//////////////////////////////////////
+/////////////////////////END 'GET' ROUTER//////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////END 'GET' ROUTER//////////////////////////////////////
+/////////////////////////END 'GET' ROUTER//////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////END 'GET' ROUTER//////////////////////////////////////
+/////////////////////////END 'GET' ROUTER//////////////////////////////////////
 
 
 
